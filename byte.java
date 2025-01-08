f43c543dd69f1dcaf8b701a408f8f90b41d96ac0
vaibhav sawant <vaibhavsawant7011@gmail.com>
2025-01-08 23:37:43 +0530

    Byte

---
+++ byte.java
@@ -0,0 +1,15 @@
+class test
+{
+	public static void main(String[]args)
+	{
+		byte b=10;
+		//byte b=127
+		//byte b=10.6;
+		//byte b=128
+		//byte b=true;
+		//byte b="Durga"
+		System.out.println(b);
+	}
+}
+
+//found the compile error which is Possible loss of precision found int,float,double,char,string required byte
