ecf6d3fe7f7cfc298eef29a394286a85ac302902
vaibhav sawant <vaibhavsawant7011@gmail.com>
2025-01-08 23:37:06 +0530

    Boolean

---
+++ boolean.java
@@ -0,0 +1,12 @@
+class booleanTest
+{
+	public static void main(String[]args)
+	{ 
+		//boolean b=true;
+		//boolean b=false;
+		//boolean b=0;        // error: incompatible types: int cannot be converted to boolean
+		//boolean b="true";  //error: incompatible types: String cannot be converted to boolean
+		boolean b=True;     //error: cannot find symbol   symbol:   variable True location: class booleanTest
+		System.out.println(b);
+        }
+}
---
+++ boolean2.java
@@ -0,0 +1,24 @@
+class boolean2
+{
+	public static void main(String[]args)
+	{ 
+		int x=10;
+		if(x) // error: incompatible types: int cannot be converted to boolean
+		{
+				System.out.println("Hello");
+		}
+		else
+		{
+				System.out.println("HI");
+		}
+
+
+
+		//or
+		//while(1) //error: incompatible types: int cannot be converted to boolean
+		//{
+			//System.out.println("HI");
+		//}
+	
+        }
+}
