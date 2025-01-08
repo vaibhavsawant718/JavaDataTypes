430eb26f08b44d4919f5add1af0b468fc5a85f42
vaibhav sawant <vaibhavsawant7011@gmail.com>
2025-01-08 23:38:23 +0530

    Char

---
+++ char.java
@@ -0,0 +1,13 @@
+class chartest
+{
+	public static void main(String[]args)
+	{
+		char ch1=97;
+		char ch2=98;
+		//char ch3=65536; //error: incompatible types: possible lossy conversion from int to char
+		System.out.println(ch1); // op a
+		System.out.println(ch2); //op b
+		//System.out.println(ch3);
+		
+	}
+}
