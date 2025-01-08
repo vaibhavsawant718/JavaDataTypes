2b6b09fd1b6a17fd76082174df89103c1896f82c
vaibhav sawant <vaibhavsawant7011@gmail.com>
2025-01-08 23:39:31 +0530

    Long

---
+++ long.java
@@ -0,0 +1,12 @@
+
+class LongTest
+{
+	public static void main(String[]args)
+	{
+		//long l=3276712345678890; //CE -> //integer number too large
+		
+		long l=3276712345678890L;
+		System.out.println(l);
+	}
+}
+
